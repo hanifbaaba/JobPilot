826d258e56538fb065e28b8bc531c97ae3028143
hanifubaaba <baabahanif88@gmail.com>
2025-07-28 22:55:42 +0100

    created JobPage and JobCard with dummy text

---
+++ frontend/src/app/Pages/JobCard/page.jsx
@@ -0,0 +1,8 @@
+export default function JobCard() {
+  return (
+    <div>
+      <h1>This is the job card</h1>
+      <p>this is the page that will show you a single job</p>
+    </div>
+  );
+}
---
+++ frontend/src/app/Pages/JobPage/page.jsx
@@ -0,0 +1,8 @@
+export default function JobPage() {
+  return (
+    <div>
+      <h1>This is the job page. </h1>
+      <p>make the job page the component that shows all jobs</p>
+    </div>
+  );
+}
