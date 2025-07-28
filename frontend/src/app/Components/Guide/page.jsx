13c913b797c04052181f0b74295016262d692a2a
hanifubaaba <baabahanif88@gmail.com>
2025-07-28 23:21:47 +0100

    created guide component

---
+++ frontend/src/app/Components/Guide/page.jsx
@@ -0,0 +1,38 @@
+export default function Guide() {
+  return (
+    <div>
+      <h1>How JobPilot works</h1>
+      <div>
+        <h1>Create Account</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis
+          repellendus consequuntur error nulla facere omnis eaque voluptatem id
+          tenetur cumque natus soluta, neque similique aliquam vel, porro
+          dolores consequatur. Saepe?
+        </p>
+        <br />
+        <h1>Upload CV/Resume</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          officiis, deserunt quas cumque voluptatibus, recusandae quasi sequi
+          adipisci corporis error nesciunt ab labore facilis saepe, tenetur
+          numquam a sapiente provident?
+        </p>
+        <h1>Find Suitable Job</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          officiis, deserunt quas cumque voluptatibus, recusandae quasi sequi
+          adipisci corporis error nesciunt ab labore facilis saepe, tenetur
+          numquam a sapiente provident?
+        </p>
+        <h1>Apply Job</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          officiis, deserunt quas cumque voluptatibus, recusandae quasi sequi
+          adipisci corporis error nesciunt ab labore facilis saepe, tenetur
+          numquam a sapiente provident?
+        </p>
+      </div>
+    </div>
+  );
+}
