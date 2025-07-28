d543742400c8b56a0b79e929011c9ec82f85e3ea
hanifubaaba <baabahanif88@gmail.com>
2025-07-28 23:16:32 +0100

    created Applications page and Vacancies component

---
+++ frontend/src/app/Components/Vacancies/page.jsx
@@ -0,0 +1,33 @@
+export default function Vacancies() {
+  return (
+    <div>
+      <h1>Most Popular Vacancies</h1>
+      <ul>
+        <li>
+          <h1>Fullstack Developer</h1>
+          <p>1,576 open positions</p>
+        </li>
+        <li>
+          <h1>Frontend Developer</h1>
+          <p>45,904 open positions</p>
+        </li>
+        <li>
+          <h1>Backend Developer</h1>
+          <p>74,857 open positions</p>
+        </li>
+        <li>
+          <h1>Javascript Developer</h1>
+          <p>61,319 open positions</p>
+        </li>
+        <li>
+          <h1>UI/UX Designer</h1>
+          <p>50,364 open positions</p>
+        </li>
+        <li>
+          <h1>Engineering manager</h1>
+          <p>3 open positions</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ frontend/src/app/Pages/Applications/page.jsx
@@ -0,0 +1,24 @@
+export default function Applications() {
+  return (
+    <section>
+      <ul>
+        <li>
+          <h3>175,324</h3>
+          <p>Live Jobs</p>
+        </li>
+        <li>
+          <h3>97,354</h3>
+          <p>Companies</p>
+        </li>
+        <li>
+          <h3>3,847,154</h3>
+          <p>Candidates</p>
+        </li>
+        <li>
+          <h3>7,632</h3>
+          <p>New Jobs</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
