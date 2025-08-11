1fc1c2179b6b24b904ce4223ce720b883d345c0a
hanifubaaba <baabahanif88@gmail.com>
2025-08-11 01:02:12 +0100

    Created DB Models for Job

--- backend/JobPilot/models.py
+++ backend/JobPilot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+      job_title = models.CharField(max_length=100)
+      description = models.TextField()
+      salary = models.DecimalField(max_digits=10, decimal_places=2)
+      company_name = models.CharField(max_length=100)
+      location = models.CharField(max_length=100)
+      category = models.CharField(max_length=100)
